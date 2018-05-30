762c5ac167d40b8ee3409f1933d6f11634012e5f
Pablo Rodriguez Carmona <p.rodriguez@merixstudio.com>
2018-05-30 11:56:33 +0200

    first commit

---
+++ app/utils.py
@@ -0,0 +1,9 @@
+import random
+
+
+def generate_random_band_name():
+    with open('app/wi_words.txt', 'r') as file:
+        words_list = file.read().split()
+        random_word = random.choice(words_list)
+    generated_band_name = random_word.replace('wi', 'weed')
+    return generated_band_name
