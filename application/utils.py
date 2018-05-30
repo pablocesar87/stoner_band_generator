225b61c3bf3377487bb5f1c6d438a859894f1f98
pablocesar87 <pablocesar87@gmail.com>
2018-05-30 15:05:06 +0200

    Add settings, make full path to wi_words.txt

--- application/utils.py
+++ application/utils.py
@@ -1,8 +1,11 @@
+import os
 import random
+
+from .settings import APP_ROOT
 
 
 def generate_random_band_name():
-    with open('application/wi_words.txt', 'r') as file:
+    with open(os.path.join(APP_ROOT, 'wi_words.txt'), 'r') as file:
         words_list = file.read().split()
         random_word = random.choice(words_list)
     generated_band_name = random_word.replace('wi', 'weed')
